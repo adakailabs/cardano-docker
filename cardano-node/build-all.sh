d168bf5ff761271eda410383d2033567bb7fa141
Luis Garcia <luis.a.garcia@hpe.com>
2021-01-28 16:23:27 -0600

    chg adjusting prometheus

--- cardano-node/build-all.sh
+++ cardano-node/build-all.sh
@@ -5,5 +5,8 @@ cd ../cardano-relay
 ./build.sh &&
 
 # create image that will support 2 relays and 1 producer
-cd ../cardano-monitor
+cd ../rt-view
+./build.sh && cd ..
+
+cd ../prometheus
 ./build.sh && cd ..
