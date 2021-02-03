641f5eeaa5dc2c6cfee42baf2d8764322a0c91a3
Luis Garcia <lagarciag@gmail.com>
2021-02-03 22:23:11 +0000

    fixing secrets permission issue

--- cardano-node/stack/deploy_node.sh
+++ cardano-node/stack/deploy_node.sh
@@ -8,8 +8,8 @@ CARDANO_REALY_VERSION=$(cat ../cardano-relay/version)
 PROMETHEUS_VERSION=$(cat ../prometheus/version)
 CARDANO_RT_VIEW_VERSION=$(cat ../rt-view/version)
 
-#docker pull "adakailabs/cardano-relay:$CARDANO_REALY_VERSION"
-#docker pull "adakailabs/cardano-monitor:$CARDANO_RT_VIEW_VERSION"
-#docker pull "adakailabs/cardano-prometheus:$PROMETHEUS_VERSION"
+docker pull "adakailabs/cardano-relay:$CARDANO_REALY_VERSION"
+docker pull "adakailabs/cardano-monitor:$CARDANO_RT_VIEW_VERSION"
+docker pull "adakailabs/cardano-prometheus:$PROMETHEUS_VERSION"
 
 docker stack deploy -c node-stack.yaml cardano
--- cardano-node/stack/services-stop.sh
+++ cardano-node/stack/services-stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker service rm cardano_monitor cardano_prometheus cardano_relay0 cardano_relay1 cardano_producer0
+
+
+docker service rm cardano_grafana cardano_monitor cardano_prometheus cardano_relay0 cardano_relay1 cardano_producer0
+docker secret rm cardano_grafana_admin_password cardano_kes_key.skey cardano_my_secret cardano_operational_certificate.cert cardano_vrf_skey.skey 
 docker service ls
+docker secret ls 
