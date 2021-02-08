20b5c702beebcc45d85d7c1044819d8a728e3b6c
Luis Garcia <lagarciag@gmail.com>
2021-02-08 21:06:18 +0000

    updating node-setup

--- cardano-node/stack/node-setup.sh
+++ cardano-node/stack/node-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-docker node update --label-add com.adakailabs.monitor=true adakai-swarm00
-docker node update --label-add com.adakailabs.relay0=true adakai-swarm01
-docker node update --label-add com.adakailabs.relay1=true adakai-swarm01
-docker node update --label-add com.adakailabs.producer0=true adakai-swarm02
+#docker node update --label-add com.adakailabs.monitor=true adakai-swarm00
+#docker node update --label-add com.adakailabs.relay0=true adakai-swarm01
+#docker node update --label-add com.adakailabs.relay1=true adakai-swarm01
+#docker node update --label-add com.adakailabs.producer0=true adakai-swarm02
+
+docker node update --label-add com.adakailabs.monitor=true master00
+docker node update --label-add com.adakailabs.relay0=true relay00
+docker node update --label-add com.adakailabs.relay1=true relay01
+docker node update --label-add com.adakailabs.producer0=true producer00
