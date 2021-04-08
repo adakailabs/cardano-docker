9e42e3dfc1ca438a62adeef64d6f1cec29e3968f
Luis Garcia <lagarciag@gmail.com>
2021-04-08 14:59:52 +0000

    updating testnet

--- cardano-node/stack/deploy_testnet.sh
+++ cardano-node/stack/deploy_testnet.sh
@@ -12,4 +12,6 @@ docker pull "adakailabs/cardano-relay:$CARDANO_REALY_VERSION"
 docker pull "adakailabs/cardano-monitor:$CARDANO_RT_VIEW_VERSION"
 docker pull "adakailabs/cardano-prometheus:$PROMETHEUS_VERSION"
 
+mkdir -p /home/lovelace/prometheus
+
 docker stack deploy -c node-testnet.yaml cardano
