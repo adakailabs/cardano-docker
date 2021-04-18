b7025e694e7fd54cf640ddb4787acff079faec0e
Luis Garcia <lagarciag@gmail.com>
2021-04-18 02:20:40 +0000

    chg: dev: fixing stop

--- cardano-node/stack/stop-all.sh
+++ cardano-node/stack/stop-all.sh
@@ -6,11 +6,6 @@ docker secret rm cardano_grafana_admin_password \
        cardano_node_kes.key \
        cardano_node.cert \
        cardano_node_vrf.key \
-       cardano_producer_private_host_name \
-       cardano_relay0_private_host_name \
-       cardano_relay0_public_host_name \
-       cardano_relay1_private_host_name \
-       cardano_relay1_public_host_name \
        cardano_gocnode.yaml 
 
 docker service ls
