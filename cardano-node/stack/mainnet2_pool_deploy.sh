5c898bfca652961c6770e56da32b7d5518d799f6
Luis Garcia <lagarciag@gmail.com>
2021-04-28 18:02:24 +0000

    updating mainet 2

---
+++ cardano-node/stack/mainnet2_pool_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e 
+set -o errexit
+set -o nounset
+
+CARDANO_REALY_VERSION=$(cat ../cardano-pool/version)
+PROMETHEUS_VERSION=$(cat ../prometheus/version)
+CARDANO_RT_VIEW_VERSION=$(cat ../rt-view/version)
+
+mkdir -p /home/lovelace/prometheus
+
+docker stack deploy -c mainnet2-pool.yaml cardano
---
+++ cardano-node/stack/stop-mainet2-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker service rm cardano_grafana cardano_monitor cardano_prometheus cardano_relay0 cardano_relay1
+
+docker secret rm cardano_grafana_admin_password \
+       cardano_gocnode.yaml 
+
+docker service ls
+
+docker secret ls 
