16882cabd0f4e47feaff0150be7b6c248f351c8b
Luis Garcia <lagarciag@gmail.com>
2021-08-30 22:42:07 +0000

    local editings

--- cardano-node/stack/stop-testnet.sh
+++ cardano-node/stack/stop-testnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker service rm cardano_monitor cardano_relay0 cardano_relay1 cardano_producer0 cardano_grafana cardano_prometheus cardano_redis 
+docker service rm cardano_monitor cardano_relay0 cardano_relay1 cardano_producer0 cardano_producer1 cardano_grafana cardano_prometheus cardano_redis 
 
 docker secret rm cardano_grafana_admin_password \
        cardano_node_kes.key \
@@ -8,7 +8,6 @@ docker secret rm cardano_grafana_admin_password \
        cardano_node_vrf.key \
        cardano_gocnode.yaml 
  
-
 docker service ls
 
 docker secret ls 
