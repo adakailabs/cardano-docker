72e52e9bb2c1b2e222bfb3d625952b9f7dfb50d9
Luis Garcia <lagarciag@gmail.com>
2021-09-23 22:57:48 +0000

    adding relay 3

--- cardano-node/stack/stop-mainet2-all.sh
+++ cardano-node/stack/stop-mainet2-all.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker service rm cardano_grafana cardano_monitor cardano_prometheus cardano_relay0 cardano_relay1 cardano_relay2 cardano_optimizer
+docker service rm cardano_grafana cardano_monitor cardano_prometheus cardano_relay0 cardano_relay1 cardano_relay2 cardano_optimizer cardano_producer0
 
-docker secret rm cardano_grafana_admin_password \
-       cardano_gocnode.yaml 
+docker secret rm cardano_grafana_admin_password cardano_gocnode.yaml 
 
 docker service ls
 
